2bf21d4c069b7a55e08920cdc9617d3554aa3cdf
Konovalov Vadim <Konovalov-Vadim2008@yandex.ru>
2024-07-19 12:36:54 +0300

    Добавление кастомных страниц ошибок

--- opinions_app.py
+++ opinions_app.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 from random import randrange
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, flash, abort
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, TextAreaField, URLField
@@ -43,7 +43,7 @@ class OpinionForm(FlaskForm):
 def index_view():
     quantity = Opinion.query.count()
     if not quantity:
-        return 'В базе данных мнений о фильмах нет'
+        abort(404)
 
     offset_value = randrange(quantity)
     opinion = Opinion.query.offset(offset_value).first()
@@ -54,7 +54,11 @@ def index_view():
 def add_opinion_view():
     form = OpinionForm()
     if form.validate_on_submit():
-        opinion = Opinion (
+        text = form.text.data
+        if Opinion.query.filter_by(text=text).first() is not None:
+            flash('Такое мнение уже было составлено ранее!')
+            return render_template('add_opinion.html', form=form)
+        opinion = Opinion(
             title=form.title.data,
             text=form.text.data,
             source=form.source.data
@@ -69,6 +73,18 @@ def add_opinion_view():
 def opinion_view(id):
     opinion = Opinion.query.get_or_404(id)
     return render_template('opinion.html', opinion=opinion)
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_error(error):
+    db.session.rollback()
+    return render_template('500.html'), 500
+
 
 
 if __name__ == '__main__':
